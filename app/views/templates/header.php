2938930be5f9a96f2485b63ec858ea70375ed8b0
Yadhi Huamani <167116887+Yadhi12Miku8@users.noreply.github.com>
2025-12-15 17:12:00 -0500

    video subido

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -19,6 +19,9 @@ header("Expires: 0");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'Panel - SamyGlow' ?></title>
+    <link rel="icon" type="image/png" href="../public/assets/img/logo.png"/>
+    <link rel="shortcut icon" type="image/png" href="../public/assets/img/logo.png"/>
+    <link rel="apple-touch-icon" href="../public/assets/img/logo.png"/>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
